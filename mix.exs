dffab23b6212e3039736601a2bad2c8612310f87
Raul Gracia <raulgracialario@gmail.com>
2021-12-28 10:20:07 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ExMedium.MixProject do
     [
       app: :ex_medium,
       name: "ExMedium",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
